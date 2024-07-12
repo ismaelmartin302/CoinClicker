936acb98dd23d01174fc9a1936e521657a082d86
isma <ismaelmartin302@gmail.com>
2024-07-13 01:56:16 +0200

    proceso creando interfaz #1

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import "@expo/metro-runtime";
 import React, { useRef, useState } from "react";
 import { StyleSheet, View, Image, Pressable, Animated } from "react-native";
-import { Text } from "react-native-web";
+import { ScrollView, Text } from "react-native-web";
 
 const coinPressableImage = require("./assets/coin.svg");
 
@@ -33,15 +33,42 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>{totalPoints}</Text>
-      <Pressable onPress={handlePress}>
-        <Animated.View
-          style={{ width: sizeInterpolation, height: sizeInterpolation }}
-        >
-          <Image source={coinPressableImage} style={styles.image} />
-        </Animated.View>
-      </Pressable>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <View style={styles.sectionStats}>
+          <Text>{totalPoints}</Text>
+        </View>
+      </View>
+      <View style={styles.main}>
+        <View style={styles.sectionPressable}>
+          <Pressable onPress={handlePress}>
+            <Animated.View
+              style={{ width: sizeInterpolation, height: sizeInterpolation }}
+            >
+              <Image source={coinPressableImage} style={styles.image} />
+            </Animated.View>
+          </Pressable>
+        </View>
+        <ScrollView style={styles.sectionElements}>
+          <View style={styles.sectionElements}>
+            <View style={styles.sectionElementsElement}></View>
+            <View style={styles.sectionElementsElement}></View>
+            <View style={styles.sectionElementsElement}></View>
+            <View style={styles.sectionElementsElement}></View>
+            <View style={styles.sectionElementsElement}></View>
+            <View style={styles.sectionElementsElement}></View>
+            <View style={styles.sectionElementsElement}></View>
+            <View style={styles.sectionElementsElement}></View>
+            <View style={styles.sectionElementsElement}></View>
+            <View style={styles.sectionElementsElement}></View>
+            <View style={styles.sectionElementsElement}></View>
+            <View style={styles.sectionElementsElement}></View>
+            <View style={styles.sectionElementsElement}></View>
+            <View style={styles.sectionElementsElement}></View>
+            <View style={styles.sectionElementsElement}></View>
+            <Text>Open up App.js to start working on your app!</Text>
+          </View>
+        </ScrollView>
+      </View>
     </View>
   );
 }
@@ -49,14 +76,55 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    gap: "5em",
     backgroundColor: "#202020",
+    width: "100%",
+    height: "100%",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  header: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+  },
+  main: {
+    width: "100%",
+    height: "100%",
+    gap: "5em",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-around",
+    flex: 4,
+  },
+  sectionStats: {
+    width: "fit-content",
+    minWidth: "200px",
+    height: "100%",
+    padding: "1em",
+    shadowRadius: 20,
+    borderBottomEndRadius: 20,
+    // shadowOpacity: 0.5,
+  },
+  sectionPressable: {},
+  sectionElements: {
+    width: "100%",
+    gap: "1em",
+    shadowRadius: 20,
+    borderRadius: 20,
+  },
+  sectionElementsElement: {
+    width: "100%",
+    minHeight: "10em",
+    padding: "1em",
+    shadowRadius: 20,
+    borderRadius: 20,
   },
   image: {
     width: "100%",
     height: "100%",
-    resizeMode: "contain",
     imageRendering: "pixelated",
+  },
+  props: {
+    resizeMode: "contain",
   },
 });
