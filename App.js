56b4afd42f90992635c32a9291980c286c288909
Ismael <ismaelmartin302@gmail.com>
2024-07-12 19:36:41 +0200

    moneda creada

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import 'bootstrap/dist/css/bootstrap.css';
-import '@expo/metro-runtime';
+import React, { useRef } from 'react';
+import { StyleSheet, View, Image, Pressable, Animated } from 'react-native';
+
+const image1 = require('./assets/favicon.png');
 
 export default function App() {
+  const animatedValue = useRef(new Animated.Value(0)).current;
+
+  const handlePress = () => {
+    Animated.sequence([
+      Animated.timing(animatedValue, {
+        toValue: 1,
+        duration: 50,
+        useNativeDriver: false,
+      }),
+      Animated.timing(animatedValue, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: false,
+      }),
+    ]).start();
+  };
+
+  const sizeInterpolation = animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: [100, 80], // in pixels
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Pressable onPress={handlePress}>
+        <Animated.View style={{ width: sizeInterpolation, height: sizeInterpolation }}>
+          <Image source={image1} style={styles.image} />
+        </Animated.View>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +42,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#202020',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+    imageRendering: 'pixelated',
   },
 });
